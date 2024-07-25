85df90952d643ea71cf2685645470268f96516bb
2342-riddhi <144895467+2342-riddhi@users.noreply.github.com>
2024-07-25 11:03:17 +0530

    1st file

---
+++ MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+  public static void main(String[] args) {
+    int x=9, y=10, sum;
+    sum = x + y;
+    System.out.println("Sum is: " + sum); // Output user input
+  }
+} 
